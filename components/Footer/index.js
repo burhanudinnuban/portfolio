31ef74742b33c8a37b2887619a07139b6a3a54ae
burhanudin nuban <burhanudinnuban@gmail.com>
2021-08-01 12:37:52 +0700

    improve UI

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div>
-      <footer className="flex items-center justify-center w-full h-24 border-t bg-gray-700 absolute">
+      <footer className="flex items-center justify-center w-full h-24 border-t bg-gray-900 absolute">
         <a
           className="flex items-center justify-center text-white"
           href="/"
--- components/Header/index.js
+++ components/Header/index.js
@@ -20,9 +20,9 @@ export default function Header() {
     );
   }
   return (
-    <div className="relative bg-gray-700 overflow-hidden">
+    <div className="relative bg-gray-900 overflow-hidden">
       <div className="max-w-7xl mx-auto">
-        <div className="abolute z-10 pb-8 bg-gray-500 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+        <div className="abolute z-10 pb-8 bg-gray-300 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-4">
             <div className="sm:text-center lg:text-left mt-10">
               <figure className="md:flex  rounded-xl p-8 md:p-0">
@@ -41,10 +41,10 @@ export default function Header() {
                     </p>
                   </blockquote>
                   <figcaption className="font-medium">
-                    <div className="text-white  text-lg">Burhanudin Nuban</div>
-                    <div className="text-gray-100">
-                      Mobile Developer, Jakarta
+                    <div className="text-purple-700  text-lg">
+                      Burhanudin Nuban
                     </div>
+                    <div className="text-black">Mobile Developer, Jakarta</div>
                   </figcaption>
                 </div>
               </figure>
@@ -79,7 +79,7 @@ export default function Header() {
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 ">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="./bg-header.jpg"
+          src="./bg-header.png"
           alt=""
         />
       </div>
