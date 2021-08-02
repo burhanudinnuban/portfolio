8a35aaa66323dcf88513c12528581abd831e0a30
burhanudin nuban <burhanudinnuban@gmail.com>
2021-08-02 16:16:09 +0700

    add go to top button

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   Banner,
   Footer,
@@ -8,6 +9,33 @@ import {
 } from "../components";
 import { NavBar } from "../components/NavBar";
 export default function Home() {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisible);
+    return () => {
+      window.removeEventListener("scroll", toggleVisible);
+    };
+  }, []);
+
+  const toggleVisible = () => {
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+      /* you can also use 'auto' behaviour
+         in place of 'smooth' */
+    });
+  };
+
   return (
     <div className="bg-gray-300">
       <Head page={"Home"} />
@@ -17,6 +45,11 @@ export default function Home() {
       <FormInformation />
       <Banner />
       <Footer />
+      {visible && (
+        <button className="fixed bottom-2 right-2" onClick={scrollToTop}>
+          <img src="/up-arrow.png" />
+        </button>
+      )}
     </div>
   );
 }
