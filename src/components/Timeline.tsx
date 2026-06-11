7bff1eef086d7c2535cb05a4615310f1f6e36f8a
Burhanudin Nuban <burhanudinnuban@gmail.com>
2026-06-11 14:48:31 +0700

    change http to https

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -231,7 +231,7 @@ export default function Timeline({ experiences, showCredentials = false, rightCo
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
+                xmlns="https://www.w3.org/2000/svg"
               >
                 <path
                   strokeLinecap="round"
